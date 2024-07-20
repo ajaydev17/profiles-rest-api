e7e9b2f5ea60b235b1bb4ad49a6799d522ec93d7
Ajay Dev <devadigaajay1729@gmail.com>
2024-07-21 01:28:42 +0530

    Added API methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.views import APIView # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework import status # type: ignore
+from .serializers import HelloSerializer
 
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Returns the list of APIView features"""
@@ -19,3 +23,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Creates a hello message with our name"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handles updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handles a partial update of an object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handles deletion of an object"""
+
+        return Response({'method': 'DELETE'})
